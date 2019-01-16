2e4e69005b53103524d8601907fabf722ba9b0fb
Sadaie Matsudaira <sadaie@matsuri-hi.me>
2019-01-16 11:33:36 +0900

    initial release

---
+++ RustyOptionalType/RustyOptionalType.h
@@ -0,0 +1,19 @@
+//
+//  RustyOptionalType.h
+//  RustyOptionalType
+//
+//  Created by Sadaie Matsudaira on 2019/01/11.
+//  Copyright © 2019 Sadaie Matsudaira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RustyOptionalType.
+FOUNDATION_EXPORT double RustyOptionalTypeVersionNumber;
+
+//! Project version string for RustyOptionalType.
+FOUNDATION_EXPORT const unsigned char RustyOptionalTypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RustyOptionalType/PublicHeader.h>
+
+
