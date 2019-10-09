6de7c8dbfc5384fb2326712163f02023cc68d8f7
Sadaie Matsudaira <sadaie@matsuri-hi.me>
2019-10-09 10:53:11 +0900

    add platform specifications

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "OpRes",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5)
+    ],
     products: [
         .library(
             name: "OpRes",
@@ -16,5 +22,6 @@ let package = Package(
         .testTarget(
             name: "OpResTests",
             dependencies: ["OpRes"]),
-    ]
+    ],
+    swiftLanguageVersions: [ .v5 ]
 )
